73d53226ead1e7be14f60425cf81989f26b760e9
Yusuf Project <yusufforproject@gmail.com>
2024-04-22 09:08:45 +0700

    Meet 7 Fixed

---
+++ storage/framework/views/796e2915ab5fb56233e4d85bf930338f.php
@@ -0,0 +1,7 @@
+ <div class="login-brand">
+     <img src="<?php echo e(asset('img/stisla-fill.svg')); ?>"
+         alt="logo"
+         width="100"
+         class="shadow-light rounded-circle">
+ </div>
+<?php /**PATH D:\Project\Laravel\laravel-pos-backend\resources\views/components/auth-header.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/c1f947f17834e01383da519c8d759689.php
@@ -0,0 +1,4 @@
+  <div class="simple-footer">
+      Copyright &copy; Stisla 2018
+  </div>
+<?php /**PATH D:\Project\Laravel\laravel-pos-backend\resources\views/components/auth-footer.blade.php ENDPATH**/ ?>
